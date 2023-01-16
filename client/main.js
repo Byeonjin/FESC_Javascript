1d6f98fc24e7ca28822f9d0de4d81f478fa79dd3
Byeonjin <quswlstkd88@naver.com>
2023-01-16 09:18:48 +0900

    jujeob

--- client/main.js
+++ client/main.js
@@ -1,1 +1,14 @@
-console.log('aa');
+function cob(name, age){
+  return {
+    name,
+    age: age,
+  }
+}
+
+let jin = cob("진상", 29);
+
+console.log(jin);
+
+if("name" in jin){
+  console.log("있어요!");
+}
